1a2a4ac6160543e40e0a686410044d7ce5d9e3cc
Jeff Vargas Barrantes <91436004+JeffVar00@users.noreply.github.com>
2023-02-08 13:26:02 -0600

    changed data files route

--- internal/repository/player_repository.go
+++ internal/repository/player_repository.go
@@ -15,10 +15,9 @@ const (
 func (r *repo) GetPlayers(ctx context.Context, filename string) ([][]string, error) {
 
 	// LOGIC CSV READ FILE // SIMULATION OF A DATABASE EXTRACTION
-
 	// Open the CSV file
 
-	file := fmt.Sprintf("C:/Users/HP/Documents/Projects/Echo_Dummy/internal/data/%s.csv", filename)
+	file := fmt.Sprintf("internal/data/%s.csv", filename)
 
 	csvFile, err := os.Open(file)
 	if err != nil {
